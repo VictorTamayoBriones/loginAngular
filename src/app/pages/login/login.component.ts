bfc538c957c58d997fb0a3861720e11287ca350e
VictorManuelTamayo <victortamayo509@gmail.com>
2020-11-03 15:35:49 -0600

    userRemenber

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,6 +3,8 @@ import { NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { UsuarioModel } from '../../models/usuario.model';
 
+//import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,6 +13,7 @@ import { UsuarioModel } from '../../models/usuario.model';
 export class LoginComponent implements OnInit {
 
   usuario: UsuarioModel = new UsuarioModel();
+  recordarme = false;
 
   constructor( private auth: AuthService ) { }
 
@@ -21,10 +24,22 @@ export class LoginComponent implements OnInit {
   login(form: NgForm){
 
     if( form.invalid ){ return; }
-
+  /*  
+    Swal.fire({
+      title: 'Error!',
+      text: 'Do you want to continue',
+      icon: 'error',
+      confirmButtonText: 'Cool'
+    });
+*/
     this.auth.login( this.usuario )
       .subscribe( resp => {
         console.log(resp);
+
+        if( this.recordarme ){
+          localStorage.setItem('email', this.usuario.email);
+        }
+
       }, (err) =>{
         console.log(err.error.error.message);
       });
